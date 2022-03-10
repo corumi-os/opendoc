2c5e75943ac3ec4e608b48855be50f3dd5c3a4ae
xianii <jiyucheng007@gmail.com>
2022-03-10 17:57:52 +0800

    update the style

--- conf.py
+++ conf.py
@@ -77,12 +77,16 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
+html_logo = 'logo2.png'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+	'style_external_links' : True,
+	'logo_only' : True,
+	'style_nav_header_background' : '#6AB0DE'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
